5e64c522e0356971d7e0fc24b1e58154253a6ea4
Christiana Razafindrasoa <ifalianarazafindrasoa@gmail.com>
2026-04-08 00:10:59 -0700

    moved getEnvVariable inside GameUtility

---
+++ src/main/java/trail/controller/GameUtility.java
@@ -0,0 +1,21 @@
+package trail.controller;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
+
+public class GameUtility {
+    public static Optional<String> getEnvVariable(String key) {
+        try {
+            String contents = Files.readString(Path.of(GameConstants.ENV_FILE_PATH));
+            for (String line : contents.split("\n")) {
+                String[] parts = line.split("=");
+                if (parts[0].equalsIgnoreCase(key)) {
+                    return Optional.of(parts[1]);
+                }
+            }
+        } catch (IOException _) {}
+        return Optional.empty();
+    }
+}
