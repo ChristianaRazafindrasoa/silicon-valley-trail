a4eaa7bf436dc2da13aab4e58c6e30bc4ab4e7e8
Christiana Razafindrasoa <ifalianarazafindrasoa@gmail.com>
2026-04-08 00:17:55 -0700

    changed from classes to records

--- src/main/java/trail/model/City.java
+++ src/main/java/trail/model/City.java
@@ -1,31 +1,7 @@
 package trail.model;
 
-import java.util.Objects;
-
-public class City {
+public record City(String name, double latitude, double longitude) {
     private static final double EARTH_RADIUS_MILES = 3958.8;
-    private final String name;
-    private final double latitude;
-    private final double longitude;
-
-    public City(String name, double latitude, double longitude) {
-        this.name = name;
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
     public double getDistance(double latitude, double longitude) {
         double dLat = Math.toRadians(this.latitude - latitude);
         double dLon = Math.toRadians(this.longitude - longitude);
@@ -36,14 +12,5 @@ public class City {
                         Math.cos(lat1Rad) * Math.cos(lat2Rad) *
                                 Math.sin(dLon / 2) * Math.sin(dLon / 2)
         ));
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        City city = (City) o;
-        return Double.compare(latitude, city.latitude) == 0 &&
-                Double.compare(longitude, city.longitude) == 0 &&
-                Objects.equals(name, city.name);
     }
 }
--- src/main/java/trail/model/Event.java
+++ src/main/java/trail/model/Event.java
@@ -1,30 +1,25 @@
 package trail.model;
 
 import java.util.List;
+import java.util.Objects;
 
-public class Event {
-    private final String title;
-    private final List<String> options;
-    private final List<Runnable> updateStates;
-
-    public Event(String title, List<String> options, List<Runnable> updateStates) {
-        this.title = title;
-        this.options = options;
-        this.updateStates = updateStates;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public List<String> options() {
-        return options;
-    }
-
+public record Event(String title, List<String> options, List<Runnable> updateStates) {
     public void selected(int option) {
         if (option <= 0 || option > options().size()) {
             throw new IllegalArgumentException();
         }
         updateStates.get(option - 1).run();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return Objects.equals(title, event.title) && Objects.equals(options, event.options);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, options);
     }
 }
