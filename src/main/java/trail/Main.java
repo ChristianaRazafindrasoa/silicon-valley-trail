cabdcfcedef8713f602ef129f322b2041e992766
Christiana Razafindrasoa <ifalianarazafindrasoa@gmail.com>
2026-04-08 00:28:29 -0700

    added dependency injection

--- src/main/java/trail/Main.java
+++ src/main/java/trail/Main.java
@@ -1,10 +1,27 @@
 package trail;
 
-import trail.controller.GameController;
+import trail.controller.*;
+import trail.view.ConsoleIO;
+
+import java.net.http.HttpClient;
+import java.util.Random;
 
 public class Main {
+
     static void main() {
-        GameController gameController = new GameController();
+        String mapboxKey = GameUtility.getEnvVariable("MAPBOX_SECRET_KEY").orElse(null);
+        if (mapboxKey == null) {
+            System.out.println("**Warning: Mapbox secret key not set up. Running on mock data.**");
+        }
+
+        ConsoleIO io = new ConsoleIO();
+        Random random = new Random();
+        MapboxApiImpl mapbox = new MapboxApiImpl(HttpClient.newHttpClient(), mapboxKey);
+        GameEvents gameEvents = new GameEvents(mapbox, random);
+        PersistState persistState = new PersistState(GameConstants.STATE_FILE_PATH);
+
+        GameController gameController = new GameController(io, gameEvents, persistState);
         gameController.run();
     }
+
 }
