3866e9afec08c7433cf360e4d3bb30e68254a9da
Christiana Razafindrasoa <ifalianarazafindrasoa@gmail.com>
2026-04-08 00:29:10 -0700

    added more game events and controller tests

--- src/test/java/trail/controller/GameControllerTest.java
+++ src/test/java/trail/controller/GameControllerTest.java
@@ -1,55 +1,71 @@
 package trail.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import trail.model.MapboxApi;
 import trail.model.State;
+import trail.view.ConsoleIO;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameControllerTest {
-    GameController gameController = new GameController();
+
+    private GameController gameController;
+
+    @BeforeEach
+    void setUp() {
+        ConsoleIO io = new ConsoleIO();
+        MapboxApi mapbox = request -> null;
+        GameEvents gameEvents = new GameEvents(mapbox, new Random());
+        PersistState persistState = new PersistState(GameConstants.STATE_FILE_PATH);
+        this.gameController = new GameController(io, gameEvents, persistState);
+    }
 
     @Test
     void shouldNotHaveSameStateAfterTravel() {
         State state1 = new State(
-                12, 3500, 0, 65, 76);
+                12, 3500, 100, 65, 76);
         State state2 = new State(
-                12, 3500, 0, 65, 76);
+                12, 3500, 100, 65, 76);
         gameController.travel(state1);
         assertNotEquals(state1.getCurrentCity(), state2.getCurrentCity());
-        assertNotEquals(state1, state2);
+        assertTrue(state1.getCash() < state2.getCash());
     }
 
     @Test
     void shouldNotHaveSameStateAfterWork() {
         State state1 = new State(
-                12, 3500, 0, 65, 76);
+                12, 3500, 100, 65, 76);
         State state2 = new State(
-                12, 3500, 0, 65, 76);
+                12, 3500, 100, 65, 76);
         gameController.work(state1);
         assertEquals(state1.getCurrentCity(), state2.getCurrentCity());
-        assertNotEquals(state1, state2);
+        assertTrue(state1.getBattery() < state2.getBattery());
+        assertTrue(state1.getCoffee() < state2.getCoffee());
     }
 
     @Test
     void shouldNotHaveSameStateAfterPromote() {
         State state1 = new State(
-                12, 3500, 0, 65, 76);
+                12, 3500, 100, 65, 76);
         State state2 = new State(
-                12, 3500, 0, 65, 76);
+                12, 3500, 100, 65, 76);
         gameController.promote(state1);
         assertEquals(state1.getCurrentCity(), state2.getCurrentCity());
-        assertNotEquals(state1, state2);
+        assertTrue(state1.getUsers() > state2.getUsers());
     }
 
     @Test
     void shouldNotHaveSameStateAfterRecharge() {
         State state1 = new State(
-                12, 3500, 0, 65, 76);
+                12, 3500, 10, 65, 76);
         State state2 = new State(
-                12, 3500, 0, 65, 76);
+                12, 3500, 10, 65, 76);
         gameController.recharge(state1);
         assertEquals(state1.getCurrentCity(), state2.getCurrentCity());
-        assertNotEquals(state1, state2);
+        assertTrue(state1.getBattery() > state2.getBattery());
     }
 
     @Test
@@ -66,6 +82,13 @@ class GameControllerTest {
     void shouldEndGameWhenBatteryIsFlat() {
         State state = new State(
                 12, 3500, 0, 65, 76);
+        assertTrue(gameController.isEndOfGame(state));
+    }
+
+    @Test
+    void shouldEndGameWhenCashRanOut() {
+        State state = new State(
+                12, 0, 20, 65, 76);
         assertTrue(gameController.isEndOfGame(state));
     }
 
--- src/test/java/trail/controller/GameEventsTest.java
+++ src/test/java/trail/controller/GameEventsTest.java
@@ -5,40 +5,55 @@ import trail.model.Event;
 import trail.model.MapboxApi;
 import trail.model.State;
 
-import java.net.http.HttpClient;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.List;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameEventsTest {
-    private final String VALID_KEY = GameController.getEnvVariable("MAPBOX_SECRET_KEY");
-    MapboxApiImpl mapbox = new MapboxApiImpl(HttpClient.newHttpClient(), VALID_KEY);
-    GameEvents gameEvents = new GameEvents(mapbox);
 
     @Test
-    void shouldGetRandomEvent() {
+    void shouldReturnFallbackWhenApiFails() {
+        MapboxApi api = request -> {
+            throw new IOException();
+        };
+        Random random = new Random(3);
+        GameEvents gameEvents = new GameEvents(api, random);
         State state = new State(
                 12, 3500, 0, 65, 76);
-        Event event = gameEvents.getRandomEvent(new Random(), state);
-        assertNotNull(event);
+        Event actualEvent = gameEvents.coffeeShopEvent(state);
+        Event initialFallbackEvent =
+                new GameEvents(api, random).fallbackCoffeeShopEvent(state);
+        assertEquals(actualEvent, initialFallbackEvent);
     }
 
     @Test
-    void shouldGetCoffeeShopEvent() {
+    void shouldReturnFallbackWhenApiReturnsEmpty() {
+        MapboxApi api = request ->
+                new MapboxApi.MapboxApiSearchResponse(List.of());
+        Random random = new Random(3);
+        GameEvents gameEvents = new GameEvents(api, random);
+        State state = new State(
+                12, 3500, 0, 65, 76);
+        Event actualEvent = gameEvents.coffeeShopEvent(state);
+        Event initialFallbackEvent =
+                new GameEvents(api, random).fallbackCoffeeShopEvent(state);
+        assertEquals(actualEvent, initialFallbackEvent);
+    }
+
+    @Test
+    void shouldReturnEventWhenApiReturnsNonEmpty() {
+        MapboxApi api = _ ->
+                new MapboxApi.MapboxApiSearchResponse(List.of(
+                        new MapboxApi.MapboxApiSearchEntry("Dutch Bros", -121.32, 92.31)));
+        Random random = new Random(3);
+        GameEvents gameEvents = new GameEvents(api, random);
         State state = new State(
                 12, 3500, 0, 65, 76);
         Event event = gameEvents.coffeeShopEvent(state);
         assertNotNull(event);
-    }
-
-    @Test
-    void shouldBuildCoffeeShopEventWithEmptySearch() {
-        State state = new State(
-                12, 3500, 0, 65, 76);
-        MapboxApi.MapboxApiSearchResponse response = new MapboxApi
-                .MapboxApiSearchResponse(new ArrayList<>());
-        Event event = gameEvents.buildCoffeeShopEvent(state, response);
-        assertNotNull(event);
+        assertEquals(2, event.options().size());
+        assertTrue(event.options().getFirst().contains("Dutch Bros"));
     }
 }
